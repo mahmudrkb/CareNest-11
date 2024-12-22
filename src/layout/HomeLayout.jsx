850b49c78a33b8e7f01a2b6491c3b8592ce78b0c
mahmudrkb <almahmudrkb98@gmail.com>
2024-12-22 15:09:23 +0600

    questons ans section done

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -4,23 +4,25 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Banner from "../components/Banner";
 import Review from "../components/Review";
+import Question from "../components/Question";
 
 const HomeLayout = () => {
   return (
     <div className="roboto-thin">
       <header className="container mx-auto p-3 my-5">
         {" "}
-        <Navbar></Navbar>{" "}
-        <Banner></Banner>
+        <Navbar></Navbar> <Banner></Banner>
       </header>
 
       <main>
         <Outlet></Outlet>
-        
+
         <section>
-            <Review></Review>
+          <Review></Review>
         </section>
-      
+        <section>
+          <Question></Question>
+        </section>
       </main>
 
       <footer>
